ce196ca84f249092e71365555038e20c75cf88e5
senaplus373-wq <senaplus373@gmail.com>
2025-10-30 00:34:26 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,15 @@
+let x = 0;
 
+function setup() {
+  createCanvas(400, 200);
+}
+
+function draw() {
+  background(135, 206, 235); // Cielo azul
+  fill(255);
+  ellipse(x, 100, 40, 40); // “Garza”
+  x += 2;
+  if (x > width) {
+    x = 0;
+  }
+}
